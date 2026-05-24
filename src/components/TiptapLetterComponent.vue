57a28dadbec36eb613d5f3b2bbf3c95f590bfe5a
Scott Feinstein <Scott_Feinstein@yahoo.com>
2026-05-24 09:29:24 -0400

    feat: visual selection of runes in doc

--- src/components/TiptapLetterComponent.vue
+++ src/components/TiptapLetterComponent.vue
@@ -6,13 +6,19 @@ const props = defineProps(nodeViewProps)
 </script>
 
 <template>
-  <node-view-wrapper class="rune-letter">
+  <node-view-wrapper class="rune-letter" contenteditable="false">
     <LetterImage :letterID="props.node.attrs.letterID" />
   </node-view-wrapper>
 </template>
 
 <style scoped>
-:deep(div.rune-letter) {
+.rune-letter {
   display: inline-block;
+  transition: background-color 0.1s ease;
+}
+
+.ProseMirror-selectednode {
+  background-color: rgba(0, 123, 255, 0.3);
+  outline: none;
 }
 </style>
