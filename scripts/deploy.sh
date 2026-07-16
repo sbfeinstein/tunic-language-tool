6926db3a36d24bab201a60b825f91744aee247df
Scott Feinstein <Scott_Feinstein@yahoo.com>
2026-07-16 17:23:21 -0400

    feat: simple outputs in deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,7 +4,11 @@
 set -e
 
 # Build
+
+echo "Removing old build output"
 rm -rf dist
+
+echo "Building"
 npm run build
 
 # Navigate to build output
@@ -13,11 +17,13 @@ cd dist
 # Create .nojekyll to bypass Jekyll processing
 touch .nojekyll
 
+echo "Initializing git"
 git init
 git add -A
 git commit -m 'Deploy'
 
 # Deploy to gh-pages branch
+echo "Deploying via git push"
 git push -f git@github.com:sbfeinstein/tunic-language-tool.git main:gh-pages
 
 cd ..
